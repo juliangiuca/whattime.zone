65b43d8439283a8049625a5196cdcc2bdb0b2cd0
Julian Giuca <julian.giuca@gmail.com>
2015-03-10 21:10:44 -0700

    Add a slider - but hide it.

--- public/ng-app/app.js
+++ public/ng-app/app.js
@@ -1,24 +1,30 @@
 var thyme = angular.module('thyme', []);
 
-thyme.controller('indexCtrl', ["$scope",
-        function indexCtrl ($scope) {
+thyme.controller('indexCtrl', ["$scope", "$interval",
+        function indexCtrl ($scope, $interval) {
 
   var tz = jstz.determine();
   $scope.zone = tz.name();
   $scope.adjustment = '';
 
-  var calc = function (startDate) {
-    var date = startDate || new Date();
-    $scope.sanfrancisco = moment(date).tz('America/Los_Angeles').format("dddd, MMMM Do YYYY, h:mm:ss a");
-    $scope.sydney       = moment(date).tz('Australia/Sydney').format("dddd, MMMM Do YYYY, h:mm:ss a");
-    $scope.shanghai     = moment(date).tz('Asia/Shanghai').format("dddd, MMMM Do YYYY, h:mm:ss a");
+  $scope.days = []
+  _.times(6, function (day) {
+    $scope.days.push(moment().add(day, 'days').format('ddd Do MMM'));
+  });
+
+  var calc = function () {
+    var date = $scope.date || new Date();
+    $scope.sanfrancisco = moment(date).tz('America/Los_Angeles').format("dddd Do @ h:mma");
+    $scope.sydney       = moment(date).tz('Australia/Sydney').format("dddd Do @ h:mma");
+    $scope.shanghai     = moment(date).tz('Asia/Shanghai').format("dddd Do @ h:mma");
   }
-  calc();
+
+  $interval(function () { calc() }, 100);
 
   $scope.$watch('adjustment', function (oldVar, newVar) {
     if (oldVar !== newVar) {
-      var newDate = chrono.parseDate($scope.adjustment);
-      calc(newDate);
+      $scope.date = chrono.parseDate($scope.adjustment);
+      calc();
     }
   });
 }]);
--- public/thyme.js
+++ public/thyme.js
@@ -1,17 +1,20 @@
-function updateClock() {
-  var now = moment(),
-      second = now.seconds() * 6,
-      minute = now.minutes() * 6 + second / 60,
-      hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+$(function() {
+  function updateClock() {
+    var now = moment(),
+        second = now.seconds() * 6,
+        minute = now.minutes() * 6 + second / 60,
+        hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
 
-  $('#hour').css("transform", "rotate(" + hour + "deg)");
-  $('#minute').css("transform", "rotate(" + minute + "deg)");
-  $('#second').css("transform", "rotate(" + second + "deg)");
-}
+    $('#hour').css("transform", "rotate(" + hour + "deg)");
+    $('#minute').css("transform", "rotate(" + minute + "deg)");
+    $('#second').css("transform", "rotate(" + second + "deg)");
+  }
 
-function timedUpdate () {
-  updateClock();
-  setTimeout(timedUpdate, 1000);
-}
+  function timedUpdate () {
+    updateClock();
+    setTimeout(timedUpdate, 1000);
+  }
 
-timedUpdate();
+  timedUpdate();
+   $( "#draggable" ).draggable({ axis: "x", containment: "#foo" });
+});
